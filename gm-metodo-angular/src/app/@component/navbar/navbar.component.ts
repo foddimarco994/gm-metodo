593e79a62ca3466c29f6894a853cf1f111767e69
foddimarco994 <foddimarco73@libero.it>
2023-06-07 19:56:59 +0200

    aggiunto ancoraggio corso e contatti

--- gm-metodo-angular/src/app/@component/navbar/navbar.component.ts
+++ gm-metodo-angular/src/app/@component/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+
+export class NavbarComponent implements OnInit {
+
   navbarOpen = false;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+  }
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  scrollToSection(section: string) {
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
 }
