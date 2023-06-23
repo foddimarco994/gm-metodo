f046ec2ca7e0c2bae586224c401b4bb8d5d82ffa
foddimarco994 <foddimarco73@libero.it>
2023-06-23 12:52:51 +0200

    aggiunti controlli del form

--- gm-metodo-angular/src/app/@component/form/form.component.ts
+++ gm-metodo-angular/src/app/@component/form/form.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ContactFormService } from 'src/app/@service/contactForm.service';
 import { ContactForm } from 'src/app/model/form';
@@ -12,22 +13,52 @@ export class FormComponent {
     name: '',
     email: '',
     comment: '',
-    privacy: false
+    privacy: false,
+    nameTouched: false,
+    emailTouched: false
   };
 
   constructor(private contactService: ContactFormService) { }
 
   onSubmit() {
-    this.contactService.sendContactForm(this.contactForm)
-      .subscribe(
-        response => {
-          console.log('Messaggio inviato correttamente:', response);
-          // Aggiungi qui la gestione della risposta, se necessario
-        },
-        error => {
-          console.error('Errore durante l\'invio del messaggio:', error);
-          // Aggiungi qui la gestione degli errori, se necessario
-        }
-      );
+
+    // Verifica se i campi obbligatori sono stati toccati e sono validi
+    if (!this.contactForm.name || !this.contactForm.email || !this.contactForm.privacy) {
+      console.log('Errore: Compilare tutti i campi obbligatori.');
+      return; // Ferma l'esecuzione del metodo se ci sono campi mancanti
+    }
+
+    if (!this.isValidEmail(this.contactForm.email)) {
+      console.log('Errore: L\'indirizzo email non è valido.');
+      return; // Ferma l'esecuzione del metodo se l'email non è valida
+    }
+
+    this.contactService.sendContactForm(this.contactForm).subscribe({
+      next: (response) =>{console.log('Messaggio inviato correttamente:', response);
+      alert('Richiesta inviata con successo.');
+
+       // Resetta i campi compilati
+       this.contactForm.name = '';
+       this.contactForm.email = '';
+       this.contactForm.comment = '';
+       this.contactForm.privacy = false;
+    },
+    error: (error) => {
+      console.error('Errore durante l\'invio del messaggio:', error);
+
+      if (error instanceof HttpErrorResponse && error.status === 422) {
+        console.log('Errore: L\'indirizzo email non è valido.');
+        alert('L\'indirizzo email non è valido.');
+      } else {
+        // Aggiungi qui la gestione degli altri errori, se necessario
+      }
+    },
+    });
+  }
+
+  // Funzione di validazione dell'email
+  isValidEmail(email: string): boolean {
+    // Implementa la tua logica di validazione dell'email qui
+    return true; // Cambia questa riga con la tua logica di validazione
   }
 }
